ca642364d13299478d82143cfa34f86ba42f0461
Matthew McPherrin <matthew@mcpherrin.ca>
2012-07-20 21:39:02 -0400

    fix persistence stupid

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 var io = require('socket.io').listen(3000);
 var fs = require('fs');
 
-var thedrawing = [];
-var persist;
+var thedrawing = JSON.parse(fs.readFileSync('persist.json', 'ascii'));
+console.log("Drawing length " + thedrawing.length);
 
 function countusers(socket) {
   l = io.sockets.clients().length
   socket.broadcast.emit('counts', l);
 }
 
-function loaddata() {
-  thedrawing = JSON.parse(fs.readFileSync('persist.json', 'ascii'));
-};
 function persist(data) {
   thedrawing.push(data);
   fs.writeFile('persist.json', JSON.stringify(thedrawing));
 };
 
 io.sockets.on('connection', function(socket) {
-  if(!persist) {
-    loaddata();
-  }
   socket.emit('start', thedrawing);
   countusers(socket);
 
