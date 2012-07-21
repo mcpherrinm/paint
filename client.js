3c360d692ae44ae61971712ca15c234e065abd5f
Matthew McPherrin <matthew@mcpherrin.ca>
2012-07-20 21:01:04 -0400

    Add some error handling

--- client.js
+++ client.js
@@ -22,9 +22,17 @@ function getMouse(c, event) {
   return {x: clickx, y: clicky};
 }
 function start() {
+  if(typeof(io) == "undefined") {
+    console.log("No socket.io");
+    var status = document.getElementById('status');
+    status.textContent = "Failed to get socket";
+    return false
+  }
   var socket = io.connect('http://taurine.csclub.uwaterloo.ca:3000');
   socket.on('counts', function(data) { console.log(data) });
   socket.on('start', function(img) {
+    var status = document.getElementById('status');
+    status.textContent = "Connected";
     var canvas = document.getElementById('share');
     var ctx = canvas.getContext('2d');
     for(var i =0; i< img.length;i++) {
